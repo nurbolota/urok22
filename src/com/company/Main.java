5db5302395c968b46e421172c5d19f59e65a2921
Nurbol Abaikanov <Abaikanov@Nurbols-MacBook-Pro.local>
2022-06-27 05:17:17 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+          ArrayList<Integer> arrayList = new ArrayList<>();
+
+          Random random = new Random();
+
+             arrayList.add(random.nextInt(1, 100));
+        System.out.println("Tak san");
+             for (int i = 1; i < 49; i++)
+             {
+              if (i % 2 == 0)
+              {
+
+                { for (int i1 : arrayList)
+
+                    System.out.print( i+",");
+            }}}
+
+        ArrayList<Integer>arr=new ArrayList<>();
+        System.out.println("\njup san");
+            for (int j = 1; j < 49; j++) {
+                if (j % 3== 0){
+                    for (int j1:arrayList
+
+                         )
+                        System.out.print(j+" ,");{
+
+                    }
+
+               }
+             }
+            }}
